de4b268bd63a9ce48b1be75fc94974be5b5df3c8
netwp1rate <netwp1rate@pop-os.localdomain>
2023-08-04 09:55:06 -0300

    adding items to menu items with dispatch action (redux)

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,9 +1,24 @@
-import { formatCurrency } from "../../utils/helpers";
+import { formatCurrency } from "../../utils/helpers"
+import { useDispatch } from "react-redux"
+import { addItem } from "../cart/cartSlice"
 
-import Button from "../../ui/Button";
+import Button from "../../ui/Button"
 
 function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch()
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    }
+    dispatch(addItem(newItem))
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -24,11 +39,15 @@ function MenuItem({ pizza }) {
             <p className="text-sm font-medium text-stone-500">Sold out</p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
-  );
+  )
 }
 
-export default MenuItem;
+export default MenuItem
